5f06ccea4ccae822e7587f7957f9239c69d4858c
ecoe <ecoe.studio111@gmail.com>
2024-03-09 10:55:14 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from src.menu import main
+from src.menu import main as text_main
+import subprocess
+import sys
 
-main()
+mode = input("選擇模式: 1.文字模式 2.GUI模式: ")
+if mode=='1':
+    text_main()
+else:
+    subprocess.run([f"{sys.executable}", "-m", "streamlit", "run", "src/ui.py"])
